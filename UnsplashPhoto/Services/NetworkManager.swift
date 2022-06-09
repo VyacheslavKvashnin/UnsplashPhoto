107efa888fe0c4e7ce19c2977bc7a13f6c3bcd84
komplektan <komplektan@mail.ru>
2022-06-09 15:26:06 +0500

    Finished the project

--- UnsplashPhoto/Services/NetworkManager.swift
+++ UnsplashPhoto/Services/NetworkManager.swift
@@ -18,38 +18,67 @@ class NetworkManager {
     private init() {}
     
     let token = "EeIzrN0doNb4XPlVf2cb7-ObHtvadTYOXRVEXetBUgw"
-  
-    func fetchPhotos(query: String, completion: @escaping(Result<[Results], NetworkError>) -> Void) {
-
-        guard let urlRandom = URL(string: "https://api.unsplash.com/photos/random/?count=30&query=\(query)") else {
-            completion(.failure(.badURL))
-            return
-        }
-        
-        var request = URLRequest(url: urlRandom)
+    
+    private func request(searchTerm: String, completion: @escaping(Data?, Error?) -> Void) {
+        let parameters = prepareParameters(searchTerm: searchTerm)
+        let url = url(param: parameters)
+        var request = URLRequest(url: url)
+        request.allHTTPHeaderFields = prepareHeader()
         request.httpMethod = "GET"
-        request.setValue("Client-ID \(token)", forHTTPHeaderField: "Authorization")
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
+        let task = createDataTask(from: request, completion: completion)
+        task.resume()
+    }
+    
+    private func prepareHeader() -> [String: String]? {
+        var headers = [String: String]()
+        headers["Authorization"] = "Client-ID \(token)"
+        return headers
+    }
+    
+    private func prepareParameters(searchTerm: String?) -> [String: String] {
+        var parameters = [String: String]()
+        parameters["query"] = searchTerm
+        parameters["count"] = String(30)
+        return parameters
+    }
+    
+    private func url(param: [String: String]) -> URL {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.unsplash.com"
+        components.path = "/photos/random/"
+        components.queryItems = param.map {
+            URLQueryItem(name: $0, value: $1)
+        }
+        return components.url ?? URL(fileURLWithPath: "")
+    }
+    
+    private func createDataTask(from request: URLRequest, completion: @escaping(Data?, Error?) -> Void) ->
+    URLSessionDataTask {
+        return URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        }
+    }
+    
+    func fetchPhotos(query: String, completion: @escaping(Result<[Results], NetworkError>) -> Void) {
+        request(searchTerm: query) { data, error in
             if error != nil {
-                completion(.failure(.invalidData))
-                return
+                completion(.failure(.badURL))
             }
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completion(.failure(.invalidData))
-                return
-            }
+            
             guard let data = data else {
                 completion(.failure(.invalidData))
-                return }
+                return
+            }
+            
             do {
                 let result = try JSONDecoder().decode([Results].self, from: data)
-                DispatchQueue.main.async {
-                    completion(.success(result))
-                }
+                completion(.success(result))
             } catch {
                 completion(.failure(.invalidData))
             }
-        }.resume()
+        }
     }
 }
